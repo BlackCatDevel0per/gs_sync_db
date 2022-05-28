a96b3a467ad88e50c78f8ba9276ee964d849d995
BlackCatDevel0per <bcdev@mail.ru>
2022-05-29 03:38:18 +0500

    Update file

--- telegram_notifier_job.py
+++ telegram_notifier_job.py
@@ -10,8 +10,9 @@ from sheet_parser.config import tg_notify_restart_timer
 try:
 	while True:
 		# Run telegram notifier job (restart every n time from config.ini)
-		telegram_notifier_job = subprocess.call(
+		telegram_notifier_job = subprocess.Popen(
 			shlex.split(f'{sys.executable} telegram_notifier.py'))
+		telegram_notifier.join()
 		time.sleep(tg_notify_restart_timer)
 		os.killpg(os.getpgid(telegram_notifier_job.pid), signal.SIGTERM)
 		print("Telegram notifier restarted!")
